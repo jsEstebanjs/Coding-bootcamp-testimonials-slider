983968666d46ccafe065a6bd3803c92fb5df9ca1
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-07-24 21:55:06 -0500

    version finall

--- code.js
+++ code.js
@@ -1,20 +1,24 @@
-const buttonLeft = document.querySelector(".button-left");
-const buttonRigth = document.querySelector(".button-rigth");
+const buttonLeft = document.querySelectorAll(".button-left");
+const buttonRigth = document.querySelectorAll(".button-rigth");
 const mainSection= [...document.querySelectorAll(".main-section")];
 
 let value;
 
-buttonRigth.addEventListener("click", ()=>{
-changuePosition(1)
+buttonRigth.forEach(item=>{
+    item.addEventListener("click", ()=> changuePosition(1));
 })
-buttonLeft.addEventListener("click", ()=>{
-    changuePosition(-1)
+;
+buttonLeft.forEach(item=>{
+    item.addEventListener("click", ()=> changuePosition(-1) );
+
 })
+
+
 function changuePosition(chague){
 const currenElement = Number(document.querySelector(".slider-body--show").dataset.id);
-   let  value = currenElement
-    value += chague
-    if(value === 0 || value === mainSection.length+1){
+    value = currenElement;
+    value+= chague;
+    if(value === 0 || value == mainSection.length+1){
          value = value === 0 ? mainSection.length : 1;
     }
 
