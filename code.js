d72f30004dbf0f46b075b64b7d1a46c3e418363e
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-07-24 20:27:38 -0500

    version con errores de devolver y avanzar slider solo da 1

--- code.js
+++ code.js
@@ -1,2 +1,24 @@
-const buttonLeft = document.querySelector(".button-left")
-const buttonRigth = document.querySelector(".button-rigth")
+const buttonLeft = document.querySelector(".button-left");
+const buttonRigth = document.querySelector(".button-rigth");
+const mainSection= [...document.querySelectorAll(".main-section")];
+
+let value;
+
+buttonRigth.addEventListener("click", ()=>{
+changuePosition(1)
+})
+buttonLeft.addEventListener("click", ()=>{
+    changuePosition(-1)
+})
+function changuePosition(chague){
+const currenElement = Number(document.querySelector(".slider-body--show").dataset.id);
+   let  value = currenElement
+    value += chague
+    if(value === 0 || value === mainSection.length+1){
+         value = value === 0 ? mainSection.length : 1;
+    }
+
+    mainSection[currenElement-1].classList.toggle("slider-body--show");
+    mainSection[value-1].classList.toggle("slider-body--show");
+
+}
